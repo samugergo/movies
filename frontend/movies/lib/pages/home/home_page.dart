dcae84413851cc5946a18219c1ba62d16e8c0d4e
Samu Gergely <samugergely99@gmail.com>
2023-03-23 07:35:43 +0100

    fix: refact home page

--- frontend/movies/lib/pages/home/home_page.dart
+++ frontend/movies/lib/pages/home/home_page.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:movies/enums/type_enum.dart';
 import 'package:movies/main.dart';
+import 'package:movies/models/base/display_model.dart';
 import 'package:movies/pages/movie/movie_page.dart';
 import 'package:movies/pages/show/show_page.dart';
 import 'package:movies/widgets/button_switch.dart';
-import 'package:movies/widgets/containers/image_gradient_container.dart';
 import 'package:movies/widgets/filter_section.dart';
 import 'package:movies/widgets/load_button.dart';
 import 'package:provider/provider.dart';
@@ -24,7 +24,7 @@ class XContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     final appState = context.watch<MainAppState>();
 
-    List list = appState.type == TypeEnum.movie ? appState.movies : appState.shows;
+    List list = appState.type == TypeEnum.movie ? appState.movies : appState.shows;    
 
     goTo(id) {
       final Widget to = appState.type == TypeEnum.movie ? MoviePage(id: id) : ShowPage(id: id);
@@ -50,43 +50,65 @@ class XContainer extends StatelessWidget {
             ],
           ),
         ),
-        ...list.map((pair) => Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            pair[0] != null 
-            ? Padding(
-              padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 16),
-              child: InkWell(
-                borderRadius: BorderRadius.circular(10),
-                onTap: () => goTo(pair[0].id),
-                child: XImage(
-                  url: pair[0].image,
-                  width: 180,
-                  height: 270,
-                  radius: 10,
-                ),
-              ),
-            )
-            : SizedBox(),
-            pair[1] != null 
-            ? Padding(
-              padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 16),
-              child: InkWell(
-                borderRadius: BorderRadius.circular(10),
-                onTap: () => goTo(pair[1].id),
-                child: XImage(
-                  url: pair[1].image,
-                  width: 180,
-                  height: 270,
-                  radius: 10,
-                )
-              ),
-            )
-            : SizedBox(),
-          ]
+        ...list.map((pair) => _ImageRow(
+          pair: pair,
+          goTo: goTo,
         )).toList(),
         LoadButton(load: appState.loadMore),
       ]
+    );
+  }
+}
+
+class _ImageRow extends StatelessWidget {
+  final List pair; 
+  final Function goTo;
+
+  _ImageRow({
+    required this.pair,
+    required this.goTo
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        pair.isNotEmpty 
+        ? _ImageCard(model: pair[0], goTo: goTo)
+        : SizedBox(),
+        pair.length > 1 
+        ? _ImageCard(model: pair[1], goTo: goTo)
+        : SizedBox()
+      ],
+    );
+  }
+
+}
+
+class _ImageCard extends StatelessWidget {
+  final DisplayModel model;
+  final Function goTo;
+
+  _ImageCard({
+    required this.model,
+    required this.goTo
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 16),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(10),
+        onTap: () => goTo(model.id),
+        child: XImage(
+          url: model.image,
+          width: 180,
+          height: 270,
+          radius: 10,
+        ),
+      ),
     );
   }
 }
