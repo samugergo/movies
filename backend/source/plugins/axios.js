ceb2ea6f873b201d76a4a9bb2f0cf55aa0b1de2b
Samu Gergely <samugergely99@gmail.com>
2023-03-23 10:13:57 +0100

    fix: project

--- backend/source/plugins/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export default axios.create({
-  baseURL: process.env.BASE_URL
-});
--- backend/source/utils/util.js
+++
@@ -1,6 +0,0 @@
-export function imageLink(image) {
-  if (image) {
-    return process.env.IMAGE_URL + image;
-  }
-  return null;
-}
