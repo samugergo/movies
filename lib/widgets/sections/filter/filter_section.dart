3f0741755d60baeca9030a1e1e820ecb03f68c5e
Samu Gergely <samugergely99@gmail.com>
2023-03-31 13:41:40 +0200

    fix: refact

--- lib/widgets/sections/filter/filter_section.dart
+++ lib/widgets/sections/filter/filter_section.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:movies/main.dart';
-import 'package:movies/widgets/filter_sheet.dart';
+import 'package:movies/widgets/sections/filter/filter_sheet.dart';
 import 'package:provider/provider.dart';
 
 class FilterSection extends StatelessWidget {
