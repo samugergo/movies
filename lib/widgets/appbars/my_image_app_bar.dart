b318c9b1e09807aad3962da93d93604e5af3e0fa
Samu Gergely <samugergely99@gmail.com>
2023-04-03 13:44:54 +0200

    fix: line fickering

--- lib/widgets/appbars/my_image_app_bar.dart
+++ lib/widgets/appbars/my_image_app_bar.dart
@@ -41,7 +41,7 @@ class MyImageAppBar extends SliverPersistentHeaderDelegate {
           top: 0 - shrinkOffset/2,
           left: 0.0,
           right: 0.0,
-          height: height,
+          height: height - 50,
           child: Opacity(
             alwaysIncludeSemantics: true,
             opacity: opacity,
@@ -101,17 +101,21 @@ class MyImageAppBar extends SliverPersistentHeaderDelegate {
             bottom: -8,
             left: 0,
             right: 0,
-            child: Opacity(
-              opacity: shrinkOffset/maxExtent,
-              child: Divider(
-                color: Colors.white,
-              ),
+            child: Divider(
+              color: color,
             ),
           ));
 
         return Container(
           decoration: BoxDecoration(
             color: color,
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: color!,
+                blurRadius: 1,
+                offset: Offset(0, 2),
+              ),
+            ],
           ),
           child: Stack(
             children: children
@@ -128,12 +132,12 @@ class MyImageAppBar extends SliverPersistentHeaderDelegate {
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           colors: [
+            color!,
             Colors.transparent, 
-            color!
           ],
         ).createShader(rect);
       },
-      blendMode: BlendMode.srcOver,
+      blendMode: BlendMode.dstIn,
       child: Container(        
         decoration: cover != null
         ? BoxDecoration(
