010adac3e04cc5ca7cb83687040ed9e714ad6f00
Samu Gergely <samugergely99@gmail.com>
2023-06-04 22:04:57 +0200

    fix: documentation

--- lib/utils/common_util.dart
+++ lib/utils/common_util.dart
@@ -6,69 +6,82 @@ import 'package:movies/theme/app_colors.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+/// This function returns the main state of the application. 
+/// This function should be used if you need this state.
+/// 
+/// [BuildContext] is the context of the build function of the widget.
 AppState getAppState(BuildContext context) {
   return context.watch<AppState>();
 }
-
+/// This function returns the main theme of the application. 
+/// This function should be used if you need the theme.
+/// 
+/// [BuildContext] is the context of the build function of the widget.
 AppColors getAppTheme(BuildContext context) {
   return Theme.of(context).extension<AppColors>()!;
 }
-
+/// This function returns the main locale of the application. 
+/// This function should be used if you need the locale.
+/// 
+/// [BuildContext] is the context of the build function of the widget.
 AppLocalizations getAppLocale(BuildContext context) {
   return AppLocalizations.of(context);
 }
-
-chunkList(list) {
-  var chunks = [];
-  int chunkSize = 2;
-  if(list != null) { 
-    for (var i = 0; i < list.length; i += chunkSize) {
-      chunks.add(list.sublist(i, i+chunkSize > list.length ? list.length : i + chunkSize)); 
-    }
-  }
-  return chunks;
-}
-
+/// This function returns a sub list of the given list. You can specify the size of the sublist.
+/// It makes sure that if the list is not longer than the given size it doesn't throw any error.
+/// 
+/// [List] is the list to make sublist from.
+/// [int] is the size of the sublist.
 sublist(List list, int count) {
   if (list.length <= count) {
     return list;
   }
   return list.sublist(0, count);
 }
-
-imageLink(image) {
+/// This function creates an image link from the given image id. There are 3 quality of each 
+/// image this function makes a link to an image with 500px width.
+/// 
+/// [String] is the id of the image
+imageLink(String? image) {
   if(image != null &&  image != '') {
     final imageUrl = dotenv.env['IMAGE_URL'];
     return '$imageUrl$image';
   }
   return '';
 }
-
-originalImageLink(image) {
+/// This function creates an image link from the given image id. There are 3 quality of each 
+/// image this function makes a link to an image with the original quality.
+/// 
+/// [String] is the id of the image
+originalImageLink(String? image) {
   if(image != null &&  image != '') {
     final imageUrl = dotenv.env['IMAGE_URL_ORIGINAL'];
     return '$imageUrl$image';
   }
   return '';
 }
-
-lowImageLink(image) {
+/// This function creates an image link from the given image id. There are 3 quality of each 
+/// image this function makes a link to an image with the lowest quality possible.
+/// This function is used when you need color calculations from an image.
+/// 
+/// [String] is the id of the image
+lowImageLink(String? image) {
   if(image != null && image != '') {
     final imageUrl = dotenv.env['IMAGE_URL_LOW'];
     return '$imageUrl$image';
   }
   return '';
 }
-
-percentFormat(number) {
-  final percent = (number * 10).round();
-  return '$percent%';
-}
-
+/// This function returns the first not null element of the given list.
+/// It assumes that the list has a not null element!
+/// 
+/// [List] is the list of the elements to check.
 getFirstNotNull(List args) {
   return args.firstWhere((element) => element != null);
 }
-
+/// This function returns the year from the given date.
+/// 
+/// [String] is the date.
 year(String date) {
   if(date != '') {
     final DateTime dateTime = DateTime.parse(date);
@@ -76,7 +89,9 @@ year(String date) {
   }
   return '';
 }
-
+/// This function format the given to a custom format.
+/// 
+/// [String] is the date to format.
 dateFormat(date) {
   if(date != '') {
     final DateFormat formatter = DateFormat('yyyy.MM.dd');
@@ -84,7 +99,10 @@ dateFormat(date) {
   }
   return '';
 }
-
+/// This function splits the given minutes into hours and minutes.
+/// 
+/// [int] is the number of minutes.
+/// [AppLocalizations] locale of the app.
 timeFormat(int minutes, AppLocalizations locale) {
   final hours = minutes ~/ 60;
   final mins = minutes % 60;
--- lib/utils/navigation_util.dart
+++ lib/utils/navigation_util.dart
@@ -2,10 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
+/// This function is the default implementation for the routing. This should be used
+/// when you need to move to a different page.
+/// 
+/// [BuildContext] is the context of the build function.
+/// [Widget] is the new page you want to go to.
 void goTo(BuildContext context, Widget to) {
   Navigator.push(
     context,
-    // CupertinoPageRoute(builder: (builder) => to)
     PageTransition(
       type: PageTransitionType.fade,
       child: to,
